35f3f80f2b4b1dae40d6450aeb47c7cea201e9b1
jennycode7 <jennessecosmas7@gmail.com>
2023-06-25 15:47:01 -0700

    Printf

---
+++ main.h
@@ -0,0 +1,39 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* for myprintf.c */
+int _printf(const char *format, ...);
+
+int checker(va_list, const char *);
+int format_specifiers(const char *, va_list, int *);
+
+/* function for format identifier */
+int puts_string(char *str);
+int for_char(va_list ap);
+int for_string(va_list ap);
+int for_int_d(va_list ap);
+int for_int_i(va_list ap);
+
+/* tools */
+char *converter(int, int);
+void reverse(char *s);
+int printInteger(int num);
+/* struct prototype */
+
+/**
+  * struct specifiers - struct for function pointer
+  * @symbol: for characters
+  * @func:  for function pointers
+  */
+typedef struct specifiers
+{
+char symbol;
+int (*func)(va_list);
+} spec;
+
---
+++ printers.c
@@ -0,0 +1,91 @@
+#include "main.h"
+
+/**
+ * for_char - prints char
+ * @ap: a va_list variable
+ * Return: returns 1
+ */
+int for_char(va_list ap)
+{
+	int sym;
+
+	sym = va_arg(ap, int);
+
+	putchar(sym);
+
+	return (1);
+}
+
+/**
+ * for_string - prints a string
+ * @ap: va_list
+ * Return: length
+ */
+int for_string(va_list ap)
+{
+	char *str;
+	int length;
+
+	str = va_arg(ap, char *);
+	if (str)
+	{
+		length = puts_string(str);
+	}
+	else
+	{
+		length = puts_string("(nil)");
+	}
+	return (length);
+}
+
+/**
+ * puts_string - prints a string
+ * @str: string
+ * Return: returns length
+ */
+int puts_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		putchar(str[i]);
+	}
+
+	return (i);
+}
+
+/**
+ * for_int_d - for d
+ * @ap: va_list
+ * Return: returns length
+ */
+int for_int_d(va_list ap)
+{
+	signed int d;
+	int length;
+
+	d = va_arg(ap, signed int);
+
+	length = printInteger(d);
+
+	return (length);
+}
+
+/**
+ * for_int_i - for i
+ * @ap: va_list
+ * Return: returns length
+ */
+int for_int_i(va_list ap)
+{
+	int i;
+	char *str;
+
+	i = va_arg(ap, int);
+	str = converter(i, 10);
+
+	i = puts_string((str != NULL) ? str : "(nil)");
+
+	return (i);
+}
